51944f2f0b9678c6fedb811b3348ac47464f9dea
Starunov <35844513@bk.ru>
2023-06-15 17:16:13 +0300

    completed hw1

---
+++ Lesson_1_Starunov/run.py
@@ -0,0 +1,11 @@
+from wsgiref.simple_server import make_server
+
+from buble_framework.main import Framework
+from urls import routes
+
+
+application = Framework(routes)
+
+with make_server('', 8000, application) as httpd:
+    print("Запуск на порту 8000...")
+    httpd.serve_forever()
